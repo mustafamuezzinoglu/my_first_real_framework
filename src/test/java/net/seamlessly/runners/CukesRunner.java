a783a3d4b2ee30fc522342b0e21fe93ab67a5307
Mustafa <mustafamuezzinoglu@gmail.com>
2023-03-25 13:52:51 +0300

    tags changed for jenkins

--- src/test/java/net/seamlessly/runners/CukesRunner.java
+++ src/test/java/net/seamlessly/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "net/seamlessly/step_definitions",
         dryRun = false,
-        tags = "@out",
+        tags = "@wip and @out",
         publish = true
 )
 public class CukesRunner {
